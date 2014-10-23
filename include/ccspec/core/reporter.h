5beda536f2d3ca455e2b494fc84250256e2f47ca
Su Zhang <me@zhang.su>
2014-10-23 18:10:11 -0400

    Pass the exception to reporter

--- include/ccspec/core/reporter.h
+++ include/ccspec/core/reporter.h
@@ -1,6 +1,7 @@
 #ifndef CCSPEC_CORE_REPORTER_H_
 #define CCSPEC_CORE_REPORTER_H_
 
+#include <exception>
 #include "example.h"
 
 namespace ccspec {
@@ -9,7 +10,7 @@ namespace core {
 class Reporter {
 public:
     void examplePassed(const Example& example) const;
-    void exampleFailed(const Example& example) const;
+    void exampleFailed(const Example& example, const std::exception&) const;
 };
 
 } // namespace core
--- src/core/reporter.cc
+++ src/core/reporter.cc
@@ -1,13 +1,17 @@
+#include <exception>
 #include <ccspec/core/example.h>
 #include <ccspec/core/reporter.h>
 
 namespace ccspec {
 namespace core {
 
+using std::exception;
+
 void Reporter::examplePassed(const Example& example) const {
 }
 
-void Reporter::exampleFailed(const Example& example) const {
+void Reporter::exampleFailed(const Example& example,
+                             const exception& exception) const {
 }
 
 } // namespace core
