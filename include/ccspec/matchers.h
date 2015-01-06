5dc6743c7d20f4e47f1f64b07b24d03d8cb3e126
Su Zhang <me@zhang.su>
2015-01-05 22:14:58 -0500

    Implement be matcher for object identity test

--- include/ccspec/matchers.h
+++ include/ccspec/matchers.h
@@ -1,6 +1,7 @@
 #ifndef CCSPEC_MATCHERS_H_
 #define CCSPEC_MATCHERS_H_
 
+#include "matchers/be.h"
 #include "matchers/eq.h"
 
 #endif // CCSPEC_MATCHERS_H_
---
+++ include/ccspec/matchers/be.h
@@ -0,0 +1,70 @@
+#ifndef CCSPEC_MATCHERS_BE_H_
+#define CCSPEC_MATCHERS_BE_H_
+
+#include <sstream>
+#include <string>
+#include <ccspec/matcher.h>
+#include <ccspec/support/inspect.h>
+
+// Interface.
+
+namespace ccspec {
+namespace matchers {
+
+template <typename U>
+class Be : public Matcher<Be<U>, U> {
+  public:
+    template <typename V>
+    bool match(const V& actual_value) const;
+    std::string desc() const override;
+
+  private:
+    explicit Be(const U& expected_value);
+
+    const U& expected_value_ref_;
+
+    template <typename T>
+    friend Be<T> be(const T& expected_value);
+};
+
+} // namespace matchers
+} // namespace ccspec
+
+// Implementation.
+
+namespace ccspec {
+namespace matchers {
+
+// Public methods.
+
+template <typename U>
+template <typename V>
+bool Be<U>::match(const V& actual_value) const {
+    return &actual_value == &expected_value_ref_;
+}
+
+template <typename U>
+std::string Be<U>::desc() const {
+    std::ostringstream s;
+    s << "should be " << support::inspect(expected_value_ref_);
+    return s.str();
+}
+
+// Private methods.
+
+template<typename U>
+Be<U>::Be(const U& expected_value)
+    : Matcher<Be<U>, U>(expected_value),
+      expected_value_ref_(expected_value) {}
+
+// Friend functions.
+
+template<typename U>
+Be<U> be(const U& expected_value) {
+    return Be<U>(expected_value);
+}
+
+} // namespace matchers
+} // namespace ccspec
+
+#endif // CCSPEC_MATCHERS_BE_H_
