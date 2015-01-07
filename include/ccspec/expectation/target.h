221d713483dff4911fabf74cf4c2b161ade39db9
Su Zhang <me@zhang.su>
2015-01-06 22:14:58 -0500

    Overload Target to integrate with be matcher
    Target needs to save the reference (address) of the actual value so
    that the be matcher can compare its address to that of the expected
    value.

--- include/ccspec/expectation/target.h
+++ include/ccspec/expectation/target.h
@@ -2,6 +2,7 @@
 #define CCSPEC_EXPECTATION_TARGET_H_
 
 #include <ccspec/matcher.h>
+#include <ccspec/matchers/be.h>
 #include "mismatch.h"
 
 // Interface.
@@ -15,7 +16,7 @@ class Target;
 } // namespace expectation
 
 template <typename T>
-expectation::Target<T> expect(T actual_value);
+expectation::Target<T> expect(const T& actual_value);
 
 namespace expectation {
 
@@ -25,13 +26,17 @@ class Target {
     template <typename ConcreteMatcher, typename V>
     void to(const Matcher<ConcreteMatcher, V>&) const;
 
+    template <typename V>
+    void to(const matchers::Be<V>&) const;
+
   private:
-    explicit Target(U actual_value);
+    explicit Target(const U& actual_value);
 
     const U actual_value_;
+    const U& actual_value_ref_;
 
     template <typename V>
-    friend Target<V> ccspec::expect(V actual_value);
+    friend Target<V> ccspec::expect(const V& actual_value);
 };
 
 } // namespace expectation
@@ -44,7 +49,7 @@ namespace ccspec {
 // Friend functions.
 
 template <typename V>
-expectation::Target<V> expect(V actual_value) {
+expectation::Target<V> expect(const V& actual_value) {
     return expectation::Target<V>(actual_value);
 }
 
@@ -59,10 +64,19 @@ void Target<U>::to(const Matcher<ConcreteMatcher, V>& matcher) const {
         throw Mismatch<U, ConcreteMatcher, V>(actual_value_, matcher);
 }
 
+template <typename U>
+template <typename V>
+void Target<U>::to(const matchers::Be<V>& matcher) const {
+    if (!matcher.match(actual_value_ref_))
+        throw Mismatch<U, matchers::Be<V>, V>(actual_value_ref_, matcher);
+}
+
 // Private methods.
 
 template <typename U>
-Target<U>::Target(U actual_value) : actual_value_(actual_value) {}
+Target<U>::Target(const U& actual_value)
+    : actual_value_(actual_value),
+      actual_value_ref_(actual_value) {}
 
 } // namespace expectation
 } // namespace ccspec
