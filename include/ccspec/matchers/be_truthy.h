e1adb11a7b5fe06027e5990329fcaaf6587dbb0a
Su Zhang <me@zhang.su>
2015-04-14 22:14:58 -0500

    Document BeTruthy

--- include/ccspec/matchers/be_truthy.h
+++ include/ccspec/matchers/be_truthy.h
@@ -10,20 +10,33 @@
 namespace ccspec {
 namespace matchers {
 
+// Matches if the actual value evalutes to true after applying the Boolean
+// negation operator ! twice.
+//
+// Example:
+//      expect(2 > 1).to(be_truthy);
+//      expect(-2 <= -1).to(be_truthy);
 class BeTruthy : public Matcher<BeTruthy> {
  public:
+  // Returns the singleton of BeTruthy.
   static const BeTruthy& instance();
 
+  // Returns true if the given actual value is true after applying the logical
+  // negation operator ! twice.
   template <typename T>
   bool match(const T& actual_value) const;
+
   std::string desc() const override;
 
  private:
   BeTruthy() = default;
+  // Deletes copy constructor and assignment operator to maintain singleton
+  // property.
   BeTruthy(const BeTruthy&) = delete;
   void operator =(const BeTruthy&) = delete;
 };
 
+// Points to the BeTruthy singleton.
 extern const BeTruthy& be_truthy;
 
 }  // namespace matchers
