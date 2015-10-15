ea9fe96493643ef94e374b4f8340b7343121e24e
Su Zhang <me@zhang.su>
2015-10-14 21:59:55 -0400

    Document UnexpectedThrow

--- include/ccspec/core/unexpected_throw.h
+++ include/ccspec/core/unexpected_throw.h
@@ -9,8 +9,13 @@
 namespace ccspec {
 namespace core {
 
+// Signifies unexpected exceptions thrown in CCSpec examples. When this
+// exception is thrown by CCSpec, it means that the system under test throws an
+// exception that is not expected by the test.
 class UnexpectedThrow : public ccspec::support::Exception {
  public:
+  // Constructs an UnexpectedThrow exception with the given cause, which is the
+  // unexpected exception the system under test originally throws.
   explicit UnexpectedThrow(const std::exception& cause);
 
  private:
